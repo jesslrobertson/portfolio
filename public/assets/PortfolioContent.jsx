d28487262b1ddd861b5992d3c39fea5ee756815d
Jess Robertson <jesslaurenrobertson@gmail.com>
2022-09-06 17:55:34 -0600

    more netlify cli setup

--- public/assets/PortfolioContent.jsx
+++ public/assets/PortfolioContent.jsx
@@ -1,21 +1,3 @@
-import React from 'react'
-
-
-const PortfolioContent = [
-  {
-  "projectTitle": "Voyage",
-  "description": "Voyage is built to interact with NASA's public image APIs. With Voyage, you can view the Astronomy Picture of the Day, search the image database, view random images, and view images from the James Webb Space Telescope.",
-  "projectImg": "public/assets/Voyage.png",
-  "githubLink": "https://github.com/jesslrobertson/voyage",
-  "address": "ode-to-voyager.netlify.app/"
-  },
-  {
-    "projectTitle": "Doodle-ooo",
-    "description": "Doodle-ooo is a minimalistic React paint app. With Doodle-ooo, you can draw using HTML Canvas and save your drawing to the gallery. More features coming soon!",
-    "projectImg":"public/assets/Doodle-ooo.png",
-    "githubLink": "https://github.com/jesslrobertson/doodle-ooo",
-    "address": "doodle-ooo.netlify.app/"
-    }
-]
-
-export default PortfolioContent
+version https://git-lfs.github.com/spec/v1
+oid sha256:e65bad47be74a26a6824e911a0e86ad00fc9d4114e99eff5b59248f95b9d5bed
+size 893
