aabbb5947efeb695f9dcdb360adbeffbc8e78ade
Jess Robertson <jesslaurenrobertson@gmail.com>
2022-09-07 13:57:51 -0600

    now using image links from git lfs

--- src/components/PortfolioContainer.jsx
+++ src/components/PortfolioContainer.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import PortfolioPiece from "./PortfolioPiece";
-import doodle from '../assets/doodle-ooo.png'
-import voyage from '../assets/voyage.png'
+
 
 export default function PortfolioContainer(props) {
   const { theme, projectsRef } = props;
@@ -11,7 +10,7 @@ export default function PortfolioContainer(props) {
       projectTitle: "Voyage",
       description:
         "Voyage is built to interact with NASA's public image APIs. With Voyage, you can view the Astronomy Picture of the Day, search the image database, view random images, and view images from the James Webb Space Telescope.",
-      projectImg: voyage,
+      projectImg: "https://media.githubusercontent.com/media/jesslrobertson/portfolio/main/src/assets/voyage.png",
       githubLink: "https://github.com/jesslrobertson/voyage",
       address: "ode-to-voyager.netlify.app/",
     },
@@ -19,7 +18,7 @@ export default function PortfolioContainer(props) {
       projectTitle: "Doodle-ooo",
       description:
         "Doodle-ooo is a minimalistic React paint app. With Doodle-ooo, you can draw using HTML Canvas and save your drawing to the gallery. More features coming soon!",
-      projectImg: doodle,
+      projectImg: "https://media.githubusercontent.com/media/jesslrobertson/portfolio/main/src/assets/doodle-ooo.png",
       githubLink: "https://github.com/jesslrobertson/doodle-ooo",
       address: "doodle-ooo.netlify.app/",
     },
