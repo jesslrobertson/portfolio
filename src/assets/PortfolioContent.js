f3c48d5eac6cdbe62cae0466adacefe24724e7b5
Jess Robertson <jesslaurenrobertson@gmail.com>
2022-09-07 12:18:06 -0600

    changed names and imports of images to all lower case

--- src/assets/PortfolioContent.js
+++ src/assets/PortfolioContent.js
@@ -2,14 +2,14 @@ const PortfolioContent = [
   {
   "projectTitle": "Voyage",
   "description": "Voyage is built to interact with NASA's public image APIs. With Voyage, you can view the Astronomy Picture of the Day, search the image database, view random images, and view images from the James Webb Space Telescope.",
-  "projectImg": "src/assets/Voyage.png",
+  "projectImg": "src/assets/voyage.png",
   "githubLink": "https://github.com/jesslrobertson/voyage",
   "address": "ode-to-voyager.netlify.app/"
   },
   {
     "projectTitle": "Doodle-ooo",
     "description": "Doodle-ooo is a minimalistic React paint app. With Doodle-ooo, you can draw using HTML Canvas and save your drawing to the gallery. More features coming soon!",
-    "projectImg":"src/assets/Doodle-ooo.png",
+    "projectImg":"src/assets/doodle-ooo.png",
     "githubLink": "https://github.com/jesslrobertson/doodle-ooo",
     "address": "doodle-ooo.netlify.app/"
     }
