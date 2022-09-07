40855a33982151033f8998af105c01150901573d
Jess Robertson <jesslaurenrobertson@gmail.com>
2022-09-07 11:32:13 -0600

    updated git lfs tracking

--- src/assets/PortfolioContent.js
+++ src/assets/PortfolioContent.js
@@ -1,18 +1,3 @@
-const PortfolioContent = [
-  {
-  "projectTitle": "Voyage",
-  "description": "Voyage is built to interact with NASA's public image APIs. With Voyage, you can view the Astronomy Picture of the Day, search the image database, view random images, and view images from the James Webb Space Telescope.",
-  "projectImg": "src/assets/Voyage.png",
-  "githubLink": "https://github.com/jesslrobertson/voyage",
-  "address": "ode-to-voyager.netlify.app/"
-  },
-  {
-    "projectTitle": "Doodle-ooo",
-    "description": "Doodle-ooo is a minimalistic React paint app. With Doodle-ooo, you can draw using HTML Canvas and save your drawing to the gallery. More features coming soon!",
-    "projectImg":"src/assets/Doodle-ooo.png",
-    "githubLink": "https://github.com/jesslrobertson/doodle-ooo",
-    "address": "doodle-ooo.netlify.app/"
-    }
-];
-
-export default PortfolioContent;
+version https://git-lfs.github.com/spec/v1
+oid sha256:4be0221ad83a94d9070eb5badc2fc69f0bb44bf07b346290c2aba36e7f54180e
+size 861
