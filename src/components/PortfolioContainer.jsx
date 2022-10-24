ac399ed3f87e42bfa3b6bc9203e56935d9815951
Jess Robertson <jesslaurenrobertson@gmail.com>
2022-10-24 12:04:25 -0600

    moved assets into public folder

--- src/components/PortfolioContainer.jsx
+++ src/components/PortfolioContainer.jsx
@@ -10,7 +10,7 @@ export default function PortfolioContainer(props) {
       projectTitle: "Plantae",
       description:
         "Plantae is a full-stack social app with authentication. It's designed for people who love caring for plants.",
-      projectImg: "src/assets/plantae.png",
+      projectImg: "public/assets/plantae.png",
       githubLink: "https://github.com/jesslrobertson/plantae",
       address: "plantae.cyclic.sh",
     },
@@ -18,7 +18,7 @@ export default function PortfolioContainer(props) {
       projectTitle: "Doodle-ooo",
       description:
         "Doodle-ooo is a minimalistic React paint app. With Doodle-ooo, you can draw using HTML Canvas and save your drawing to the gallery. More features coming soon!",
-      projectImg: "src/assets/doodle-ooo.png",
+      projectImg: "public/assets/doodle-ooo.png",
       githubLink: "https://github.com/jesslrobertson/doodle-ooo",
       address: "doodle-ooo.netlify.app/",
     },
@@ -26,7 +26,7 @@ export default function PortfolioContainer(props) {
       projectTitle: "Voyage",
       description:
         "Voyage is built to interact with NASA's public image APIs. With Voyage, you can view the Astronomy Picture of the Day, search the image database, view random images, and view images from the James Webb Space Telescope.",
-      projectImg: "src/assets/voyage.png",
+      projectImg: "public/assets/voyage.png",
       githubLink: "https://github.com/jesslrobertson/voyage",
       address: "ode-to-voyager.netlify.app/",
     },
